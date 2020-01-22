ff0a243147069df6565b3136cbb964516b5cf899
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-01-22 15:55:45 +0000

    add main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
+        <Main/>
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Main.css";
+
+
+const main = props => (
+  <main>
+        <h1>Selchuk Karakus</h1>
+        <p>Website & Portfolio of a Software Developer | London-based</p>
+  </main>
+);
+
+export default main;
+    
