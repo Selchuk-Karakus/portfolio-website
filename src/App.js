643d9d41c28c977d5677a3f1ab27ffceb5f16a00
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-01-23 21:18:35 +0000

    add footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar/NavBar';
 import Header from './components/Header/Header';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Header/>
         <About/>
         <Projects/>
-        {/* <Footer/> */}
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <h2>Get In Touch</h2>
+            <p>Let's grab a drink to find out if I can be of any help to you. 
+                Please don't hesitate to contact me by email or phone.
+                 Or reach out to me on: LinkedIn.</p>
+            <ul>
+                <li>LinkedIn</li>
+                <li>GitHub</li>
+                <li>Twitter</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
