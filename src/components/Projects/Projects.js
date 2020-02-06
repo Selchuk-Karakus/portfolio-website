ab0ec3fb8adffd728251e51dd8dbd1279a20526f
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-02-06 14:42:31 +0000

    touch up container

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import './projects.css';
+import React from "react";
+import "./projects.css";
 
 function Projects() {
-    return (
-        <div className='Projects'>
-            <section className='project-container'>
-            <h1>My Projects</h1>
-            </section>
-        </div>
-    );
+  return (
+    <div className="Projects">
+      <h1>My Projects</h1>
+      <section className="project-container">
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1580876205972-afbf5878d145?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1580876205972-afbf5878d145?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1580876205972-afbf5878d145?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1580876205972-afbf5878d145?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1580876205972-afbf5878d145?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1580876205972-afbf5878d145?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+      </section>
+    </div>
+  );
 }
 
 export default Projects;
