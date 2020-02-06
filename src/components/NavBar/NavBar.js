27992868f52a6dc75796371e22cddecfce0d749b
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-02-06 20:33:56 +0000

    add page scroll link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,13 +5,13 @@ import "./NavBar.css";
 function NavBar() {
   return ( 
       <nav className="navbar navbar-navigation">
-        <div className="navbar-logo"><a href="/">SK</a></div>
+        <div className="navbar-logo"><a href="#Home">SK</a></div>
         <div className='separator'></div>
         <div className="navbar-navigation-items">
           <ul>
-            <li><a href="/">About</a></li>
-            <li><a href="/">Projects</a></li>
-            <li><a href="/">Contacts</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Projects">Projects</a></li>
+            <li><a href="#Contacts">Contacts</a></li>
           </ul>
         </div>
       </nav>
