f71086e896d87af1a23750415c79e5fe9180e9af
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-02-06 14:39:38 +0000

    reword paragraph

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,23 +7,26 @@ function About() {
             <h1>About Me</h1>
             <section>
                 <p>
-                    A graduate of KODIRI, 
+                    I'm interested in I.T and everything it orbits! A graduate of KODIRI, 
                     a bootcamp teaching Full-Stack Developer focusing primarirly
                     on JavaScript, React, NodeJs, ExpressJS and MongoDB.
                 </p>
+                <hr />
                 <p>
                     Before my devotion to computer programming,
                     I did years of professional work in hospitality industry where 
                     I’ve picked up key skills including communication, teamwork and customer service.
                 </p>
+                <hr />
                 <p>
-                    I'm now exploring various roles in web development looking
+                    I'm now exploring various roles in software development looking
                     to make tech industry a home.
                 </p>
+                <hr />
                 <p>
                     As a career changer I believe I am abnormaly driven with a frightening big appetite,
                     and now want to join a team to build exciting,
-                    elegant and responsive websites and applications for any sector
+                    elegant and responsive website's and applications for any sector
                     where good user-friendly design makes a difference!
                 </p>
             </section>
