f339a428ecabd44e6b7ffbce2a9147b85b440b87
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-02-14 14:30:06 +0000

    add logo for links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import GmailLogo from '../images/gmail.svg';
+import TwitterLogo from '../images/twitter.svg';
+import GithubLogo from '../images/github.svg';
+import LinkedIn from '../images/linkedin.svg';
 import './Footer.css';
 
 function Footer() {
@@ -9,12 +13,13 @@ function Footer() {
                 Please don't hesitate to contact me by email.
                  Or reach out to me on: LinkedIn.</p>
             <ul>
-                <li><a href='https://www.linkedin.com/in/selchuk-karakus/' target='_blank'>LinkedIn</a></li>
-                <li><a href='https://github.com/Selchuck' target='_blank'>GitHub</a></li>
-                <li><a href='https://twitter.com/KarakusSelchuk' target='_blank'>Twitter</a></li>
+                <li className='zoom'><a href='https://www.linkedin.com/in/selchuk-karakus/' target='_blank'><img src={LinkedIn} /></a></li>
+                <li className='zoom'><a href='https://github.com/Selchuck' target='_blank'><img src={GithubLogo} /></a></li>
+                <li className='zoom'><a href='https://twitter.com/KarakusSelchuk' target='_blank'><img src={TwitterLogo} /></a></li>
+                <li className='zoom'><a href='mailto:selchuk.karakus@gmail.com' target='_blank'><img src={GmailLogo} /></a></li>
             </ul>
         </footer>
-    )
+    )   
 }
 
 export default Footer;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./NavBar.css";
+import Logo from '../images/logo.svg';
 
 
 function NavBar() {
   return ( 
       <nav className="navbar navbar-navigation">
-        <div className="navbar-logo"><a href="#Home">SK</a></div>
+        <div className="navbar-logo"><a href="#Home"><img src={Logo}/></a></div>
         <div className='separator'></div>
         <div className="navbar-navigation-items">
           <ul>
