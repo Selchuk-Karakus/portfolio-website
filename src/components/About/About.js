b3ec066ac9af269b7223ed1e51b5a9dc18f874cd
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-02-20 11:56:55 +0000

    add skill logo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './About.css';
+import jsLogo from '../images/js.svg';
+import computerLogo from '../images/computer.svg';
+import teamLogo from '../images/team.svg';
+import transferLogo from '../images/transfer.svg';
 
 function About() {
     return (
@@ -9,21 +13,24 @@ function About() {
                 <section>
                     <p>
                         I'm interested in I.T and everything it orbits! A graduate of KODIRI, 
-                        a bootcamp teaching <strong>Full-Stack Developer</strong> focusing primarily
+                        a bootcamp teaching <strong>Full-Stack Development</strong> focusing primarily
                         on <strong>JavaScript</strong>, <strong>React</strong>, <strong>NodeJs</strong>,
                         <strong>ExpressJS</strong> and <strong>MongoDB</strong>.
                     </p>
+                    <img src={jsLogo}/>
                     <hr />
                     <p>
                         Before my devotion to computer programming,
                         I did years of professional work in hospitality industry where 
                         I’ve picked up key skills including <strong>communication</strong>, <strong>teamwork</strong> and <strong>customer service</strong>.
                     </p>
+                    <img src={teamLogo}/>
                     <hr />
                     <p>
                         I'm now exploring various roles in software development looking
                         to make tech industry a home.
                     </p>
+                    <img src={computerLogo}/>
                     <hr />
                     <p>
                         As a career changer I believe I am abnormaly driven with a frightening big appetite,
@@ -31,6 +38,7 @@ function About() {
                         elegant and responsive website's and applications for any sector
                         where good user-friendly design makes a difference!
                     </p>
+                    <img src={transferLogo}/>
                 </section>
             </main>
         </div>
