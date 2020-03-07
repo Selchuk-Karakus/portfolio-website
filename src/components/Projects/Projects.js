ee3cd3e4880bfd5fe7d29d0079633c903e991caf
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-03-07 17:38:15 +0000

    change key value

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./projects.css";
 import ClickMe from "../../assets/images/clickMe.svg";
-import './projectsData'
+import "./projectsData";
 import projectsData from "./projectsData";
 
-function Projects() {
+function Projects({ project }) {
   return (
     <div id="Projects" className="Projects">
       <h1>My Projects</h1>
       <section className="project-container">
-        {projectsData.map((project, index) => (
-          <div key={index} className="item">
+        {projectsData.map(project => (
+          <div key={projectsData.tittle} className="item">
             <img src={project.imgUrl} alt="logo of project" />
             <div className="overlay">
               <h1>{project.tittle}</h1>
