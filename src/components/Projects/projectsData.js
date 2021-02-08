9da19a787c1b67494b5615183a52a6f68d9e6bbb
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-08 15:41:41 +0100

    add new project details to projectsData array object

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -4,7 +4,8 @@ const projectsData = [
     imgUrl:
       "https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     linkUrl: "https://zepfferson-live.herokuapp.com/",
-    description: "An e-commerce app where the user can subscribe, shop and buy goods.  Created with HTML5, CSS3, JavaScript, React and Redux."
+    description:
+      "An e-commerce app where the user can subscribe, shop and buy goods.  Created with HTML5, CSS3, JavaScript, React and Redux."
   },
   {
     tittle: "KodFlix",
@@ -15,12 +16,12 @@ const projectsData = [
       "A Netflix like platform of some of my favourite TV shows, built using JavaScript, ReactJs, Node, Express and MongoDB."
   },
   {
-    tittle: "Pub Crawl",
+    tittle: "Video Addict",
     imgUrl:
-      "https://images.unsplash.com/photo-1546622891-02c72c1537b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    linkUrl: "http://www.randomcheers.com/",
+      "https://images.unsplash.com/photo-1522401195795-1d580bb06720?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzJ8fHZpZGVvfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    linkUrl: "https://keen-boyd-ab7405.netlify.app/",
     description:
-      "A web-page that generates a pub crawl built with Ruby on Rails,Mapbox, JavaScript and Google Place’s API done over the final two weeks of Le Wagon."
+      "A web-page, which provides a search interface for finding YouTube videos based on the entered search term, and returning these videos as a detailed list. YouTube Data API demonstration. Built with React, Semantic UI and Axios library."
   },
   {
     tittle: "Robo Friends",
@@ -28,7 +29,7 @@ const projectsData = [
       "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     linkUrl: "https://reverent-allen-d1e546.netlify.com/",
     description:
-      "A single page application where the user can search for robotfriends among all. Created with HTML5, CSS3, JavaScript, React,Redux, tachyons, robohash API and jsonplaceholder API."
+      "A single page application where the user can search for robotfriends among all. Created with HTML5, CSS3, JavaScript, React, Redux, tachyons, robohash API and jsonplaceholder API."
   },
   {
     tittle: "Zappa Rentals",
@@ -44,8 +45,8 @@ const projectsData = [
       "https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     linkUrl: "http://we-pair.herokuapp.com/",
     description:
-      "An on going joint project working with four others at KODIRI. Anapp which brings those together who want to find someone who wants to do the same thing as them for the same purpose - learn programming or improve and develop their current programming skills by pair programming."
-  },
+      "An on going joint project working with four others at KODIRI. An app which brings those together who want to do the same thing for the same purpose - learn programming or improve and develop their programming skills by pair programming."
+  }
 ];
 
 export default projectsData;
