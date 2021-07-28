7265103d19315e7bb204c6dadb149fef481b03c0
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-07-28 21:48:11 +0100

    disable click-me animation on mobile device

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
     <header id="Home" className="container">
       <h1 className="header">Selchuk Karakus</h1>
       <div className="intro">
-        <p className="animated slideInLeft delay-1s slow">
+        <h2 className="animated slideInLeft delay-1s slow">
           Website & Portfolio of a Software Developer | London-based
-        </p>
+        </h2>
       </div>
       <div className="scroll-down animated flash slower infinite">
         <a href="#About">
