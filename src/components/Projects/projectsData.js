5fca1a62c745c3bdac494381b3fc0339acfe4df0
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-10-03 15:28:17 +0100

    Update: add latest project

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -1,11 +1,19 @@
 const projectsData = [
   {
+    tittle: "Carriers of  Hope",
+    imgUrl:
+      "https://images.unsplash.com/photo-1593113598332-cd288d649433?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hhcml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    linkUrl: "https://carriers-of-hope-deploy.herokuapp.com/",
+    description:
+      "A team-project built with two other co-trainee developers at CodeYourFuture during the final month of the program for a charity shop based in Coventry. The application was built from ground-up using P.E.R.N stack. I worked on Front-end and Back-end for this project as a Full-stack developer",
+  },
+  {
     tittle: "Zepfferson Clothin",
     imgUrl:
       "https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     linkUrl: "https://zepfferson-live.herokuapp.com/",
     description:
-      "An e-commerce app where the user can subscribe, shop and buy goods.  Created with HTML5, CSS3, JavaScript, React and Redux."
+      "A code-along video Udemy project built primarily to learn Redux. An e-commerce app where the user can subscribe, shop and buy goods.  Created with HTML5, CSS3, JavaScript, React and Redux.",
   },
   {
     tittle: "KodFlix",
@@ -13,7 +21,7 @@ const projectsData = [
       "https://images.unsplash.com/photo-1515634928627-2a4e0dae3ddf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     linkUrl: "http://kodflix-kodiri.herokuapp.com/",
     description:
-      "A Netflix like platform of some of my favourite TV shows, built using JavaScript, ReactJs, Node, Express and MongoDB."
+      "A Netflix like platform of some of my favourite TV shows, built using JavaScript, ReactJs, Node, Express and MongoDB.",
   },
   {
     tittle: "Video Addict",
@@ -21,7 +29,7 @@ const projectsData = [
       "https://images.unsplash.com/photo-1522401195795-1d580bb06720?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzJ8fHZpZGVvfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     linkUrl: "https://keen-boyd-ab7405.netlify.app/",
     description:
-      "A web-page, which provides a search interface for finding YouTube videos based on the entered search term, and returning these videos as a detailed list. YouTube Data API demonstration. Built with React, Semantic UI and Axios library."
+      "A web-page, which provides a search interface for finding YouTube videos based on the entered search term, and returning these videos as a detailed list. YouTube Data API demonstration. Built with React, Semantic UI and Axios library.",
   },
   {
     tittle: "Robo Friends",
@@ -29,7 +37,7 @@ const projectsData = [
       "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     linkUrl: "https://reverent-allen-d1e546.netlify.com/",
     description:
-      "A single page application where the user can search for robotfriends among all. Created with HTML5, CSS3, JavaScript, React, Redux, tachyons, robohash API and jsonplaceholder API."
+      "A single page application where the user can search for robotfriends among all. Created with HTML5, CSS3, JavaScript, React, Redux, tachyons, robohash API and jsonplaceholder API.",
   },
   {
     tittle: "Zappa Rentals",
@@ -37,16 +45,8 @@ const projectsData = [
       "https://images.unsplash.com/photo-1482442120256-9c03866de390?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     linkUrl: "http://airbnb-maxlongbao.herokuapp.com/",
     description:
-      "An Air-BnB-style platform for loaning and renting musical instruments.  App implemented searching and geocoding in Ruby on Rails"
+      "An Air-BnB-style platform for loaning and renting musical instruments.  App implemented searching and geocoding in Ruby on Rails",
   },
-  {
-    tittle: "We-Pair",
-    imgUrl:
-      "https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    linkUrl: "http://we-pair.herokuapp.com/",
-    description:
-      "An on going joint project working with four others at KODIRI. An app which brings those together who want to do the same thing for the same purpose - learn programming or improve and develop their programming skills by pair programming."
-  }
 ];
 
 export default projectsData;
